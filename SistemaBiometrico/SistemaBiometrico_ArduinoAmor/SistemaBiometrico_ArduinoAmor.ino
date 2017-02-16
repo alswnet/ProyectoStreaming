6fc89163359823cc5c8aa1b98f64b96f287503a1
chepecarlos <chepecarlos@alswblog.org>
2017-02-15 19:00:49 -0600

    Agregando la libreria al repositorio

--- SistemaBiometrico/SistemaBiometrico_ArduinoAmor/SistemaBiometrico_ArduinoAmor.ino
+++ SistemaBiometrico/SistemaBiometrico_ArduinoAmor/SistemaBiometrico_ArduinoAmor.ino
@@ -1,26 +1,30 @@
 //Librerias del Wifi, Sensor de huella y comunicasion serial UARL
 #include <SoftwareSerial.h>
 #include <SparkFunESP8266WiFi.h>
+#include <LiquidCrystal.h>
 #include "FPS_GT511C3.h"
 
 //Mombre de la red, contraseña y nombre del servidor
-const char MiRed[] = "ALSW2";
+const char MiRed[] = "ALSW 2.4Ghz";
 const char MiContra[] = "2124-1324";
-const char Servidor[] = "192.168.43.97";
+const char Servidor[] = "192.168.1.108";
 
-FPS_GT511C3 Huella(6, 7);
-
-int PinBuzzer = 10;
+//Variables globales
+FPS_GT511C3 Huella(6, 7);//Sensor de huellas
+LiquidCrystal Pantalla(11, 12, 5, 4, 3, 2);//Pantalla LCD
+int PinBuzzer = 10;//Buzzer
 
 void setup() {
   Serial.begin(9600);
   pinMode(PinBuzzer, OUTPUT);
+  ActivarLCD();
+  MostrarLCD(0);
   Alerta(0);
   ActivarWifi();
   Alerta(0);
   ActivarHuela();
   Alerta(0);
-  esp8266.listen();
+ // esp8266.listen();
 }
 
 void loop() {
@@ -37,11 +41,13 @@ void ActivarHuela() {
 void ActivarWifi() {
   while (esp8266.begin() != true) {//Verifica que encuentre el escudo o espera
     Serial.println("Error con escudo to ESP8266.");
+    MostrarLCD(1);
     delay(1000);
   }
   if (esp8266.status() <= 0) {//Verifica si esta conectado a red sino se conecta
     while (esp8266.connect(MiRed, MiContra) < 0) {//Intenta conectarse, sino espera y reintenta
       Serial.print(".");
+      MostrarLCD(1);
       delay(1000);
     }
   }
@@ -49,16 +55,21 @@ void ActivarWifi() {
   Serial.println(esp8266.localIP());//Muestra la ip del arduino
 }
 
+//Activar la LCD
+void ActivarLCD() {
+  Pantalla.begin(16, 2);
+  Pantalla.clear();
+}
+
 void BuscarHuella() {
-  Huella.listen();
-  if (Huella.IsPressFinger())
-  {
+  //Huella.listen();
+  if (Huella.IsPressFinger()) {
     Alerta(0);
     Huella.CaptureFinger(false);
     int id = Huella.Identify1_N();
+    Alerta(0);
     Huella.SetLED(false);
-    if (id < 200)
-    {
+    if (id < 200) {
       Serial.print("Encontrado ");
       Serial.println(id);
       delay(100);
@@ -68,6 +79,7 @@ void BuscarHuella() {
     }
     else {
       Serial.println("No te conosco");
+      MostrarLCD(7, "No te conosco");
       Alerta(1);
     }
     Huella.SetLED(true);
@@ -87,19 +99,21 @@ void EnviarWifi(int ID) {
     client.print(F("\nConnection: close\n\n"));
 
     int Respuesta = BuscarInt(client, 'E');
-    char Nombre = BuscarChar(client, 'N');
+    //char Nombre = BuscarChar(client, 'N');
     switch (Respuesta) {
       case 1:
         Serial.println("Adios :')");
-        Serial.println(
+        MostrarLCD(2, "Adios :')");
         Alerta(2);
         break;
       case 2:
         Serial.println("Hola Minion :D");
+        MostrarLCD(2, "Hola :D");
         Alerta(3);
         break;
       default:
         Serial.println("Quien erres @_@");
+        MostrarLCD(7, "No estas DB");
         Alerta(1);
         break;
     }
@@ -108,8 +122,34 @@ void EnviarWifi(int ID) {
   delay(100);
 }
 
+void MostrarLCD(int Mensaje, char Extra[]) {
+  MostrarLCD(Mensaje);
+  Pantalla.setCursor(0, 1);
+  Pantalla.print(Extra);
+}
+
+void MostrarLCD(int Mensaje) {
+  Pantalla.clear();
+  Pantalla.setCursor(0, 0);
+  switch (Mensaje) {
+    case 0:
+      Pantalla.print("Iniciando");
+      break;
+    case 1:
+      Pantalla.print("Error Wifi");
+      break;
+    case 2:
+      Pantalla.print("Actualizado");
+      break;
+    default:
+      Pantalla.print("Error");
+      break;
+  }
+
+}
+
 char BuscarChar(ESP8266Client client, char Buscar) {
-return "Chepecarlos";
+  return "Chepecarlos";
 }
 
 int BuscarInt(ESP8266Client client, char Buscar) {
@@ -203,3 +243,4 @@ void Alerta(int tono) {
   noTone(PinBuzzer);
   delay(250);
 }
+
