e9c23f33eebd7d14c694f981fb415438dafa7506
chepecarlos <chepecarlos@alswblog.org>
2017-03-01 19:35:22 -0600

    Agregado por chepe codigo minimo del juevo

--- TriviaArduino/TriviaArduino.ino
+++ TriviaArduino/TriviaArduino.ino
@@ -7,11 +7,13 @@ File Archivo;
 int CantidadPreguntas = 0;
 
 String Pregunta;
+String Opciones[4];
 String Opcion0;
 String Opcion1;
 String Opcion2;
 String Opcion3;
 int Respueta;
+int IDPregunta;
 
 void setup() {
   Serial.begin(9600);
@@ -21,23 +23,41 @@ void setup() {
   }
   Serial.println("SD funciona");
   CantidadPreguntas = ContrarPreguntas();
+  Serial.println();
+  Serial.println();
 }
 
 void loop() {
-  int IDPregunta = random(CantidadPreguntas);
+  IDPregunta = random(CantidadPreguntas);
   BuscarPregunta(IDPregunta);
-  Serial.print("La preguntas numero ");
-  Serial.println(IDPregunta);
-  Serial.print("Preguntas es: ");
-  Serial.println(Pregunta);
-  Serial.print("Respuesta es: ");
-  Serial.println(Respueta);
-  Serial.println();
+  MostarSerial();
   delay(1000);
 }
 
+void MostarSerial() {
+  Serial.print("ID ");
+  Serial.print(IDPregunta);
+  Serial.print(" - ");
+  Serial.println(Pregunta);
+  Serial.print("Respuesta: ");
+  Serial.println(Respueta);
+  Serial.print("Op0: ");
+  Serial.print(Opcion0);
+  Serial.print(" Op1: ");
+  Serial.print(Opcion1);
+  Serial.print(" Op2: ");
+  Serial.print(Opcion2);
+  Serial.print(" Op3: ");
+  Serial.print(Opcion3);
+  Serial.println();
+  Serial.println();
+}
 void BuscarPregunta(int ID) {
-  Pregunta = " ";
+  Pregunta = "";
+  Opcion0 = "";
+  Opcion1 = "";
+  Opcion2 = "";
+  Opcion3 = "";
   int IDBusqueda = ID;
   int Encontrado = 0;
   Archivo = SD.open("Pregun.txt");
@@ -46,10 +66,16 @@ void BuscarPregunta(int ID) {
     while (Archivo.available()) {
       char Letra = Archivo.read();
       if (Letra == '\n') {
-        if (IDBusqueda == 0) {
-          if (Letra == '\t') {
-            Encontrado++;
-          }
+        if (IDBusqueda < 0) {
+          return;
+        }
+        IDBusqueda--;
+      }
+      else if (IDBusqueda == 0) {
+        if (Letra == '\t') {
+          Encontrado++;
+        }
+        else {
           switch (Encontrado) {
             case 0:
               Pregunta = Pregunta + String(Letra);
@@ -57,21 +83,26 @@ void BuscarPregunta(int ID) {
             case 1:
               Respueta = int(Letra) - '0';
               break;
-
+            case 2:
+              Opcion0 = Opcion0 + String(Letra);
+              break;
+            case 3:
+              Opcion1 = Opcion1 + String(Letra);
+              break;
+            case 4:
+              Opcion2 = Opcion2 + String(Letra);
+              break;
+            case 5:
+              Opcion3 = Opcion3 + String(Letra);
+              break;
           }
-
-
-          return;
         }
-        IDBusqueda--;
       }
-
-      //Serial.print(Letra);
-
     }
   }
   Archivo.close();
 }
+
 int ContrarPreguntas() {
   int Preguntas = 0;
   Archivo = SD.open("Pregun.txt");
