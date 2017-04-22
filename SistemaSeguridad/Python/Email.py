e1332b27c8bc665cac66410bf136b7fd2ff93ff6
chepecarlos <chepecarlos@alswblog.org>
2017-04-22 00:06:59 -0600

    Codigo para mandar correo via python

---
+++ SistemaSeguridad/Python/Email.py
@@ -0,0 +1,43 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+#Librerias
+import time
+import serial
+import smtplib
+import mimetypes
+
+#importar modulos
+from email.MIMEText import MIMEText
+
+#Declaro el correo y contraseña
+Emisor = "Correo@emisor.com"
+Receptor = "Correo@receptor.com"
+Contrasena = "contraseña"
+
+print "Iniciando envio"
+
+#Obteniendo la fecha y hora actual 
+Fecha = time.strftime("%d/%m/%y")
+Hora = time.strftime("%H:%M:%S")
+
+#Creando mensaje a mandar
+Mensaje = MIMEText("Hola alquien se metio el "+ Fecha+ "a la hora " + Hora + " Peligro")
+Mensaje['From'] = Emisor
+Mensaje['To'] = Receptor
+Mensaje['Subject'] = "Alarma " + Fecha + " " + Hora
+
+print Mensaje
+
+#Preparando comunicacion
+mailServer = smtplib.SMTP('smtp.gmail.com',587)
+mailServer.ehlo()
+mailServer.starttls()
+mailServer.ehlo()
+mailServer.login(Emisor, Contrasena)
+
+#enviar el correo 
+mailServer.sendmail(Emisor, Receptor, Mensaje.as_string())
+print("Mensaje Enviado")
+
+mailServer.close()
