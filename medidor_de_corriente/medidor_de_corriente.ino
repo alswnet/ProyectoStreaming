a53361a7e4bb8425d280089727e0d8b16b8d6702
carl0shdz <carlitos.hernandez203@gmail.com>
2017-06-19 16:50:27 -0300

    medición de corriente funcional

--- medidor_de_corriente/medidor_de_corriente.ino
+++ medidor_de_corriente/medidor_de_corriente.ino
@@ -1,31 +1,17 @@
-//#include "EmonLib.h"                   // Include Emon Library
-//EnergyMonitor emon1;                   // Create an instance
-//
-//void setup()
-//{
-//  Serial.begin(9600);
-//
-//  emon1.current(5, 60);             // Current: input pin, calibration.
-//  //calibration is explained bellow
-//}
-//
-//void loop()
-//{
-//  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
-//
-//  Serial.print(Irms*230.0);         // Apparent power
-//  Serial.print(" ");
-//  Serial.println(Irms);          // Irms
-//}
+// EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
 
+#include "EmonLib.h"                   // Include Emon Library
+EnergyMonitor emon1;                   // Create an instance
 
-int medida = 0;
-void setup(){
+void setup()
+{  
   Serial.begin(9600);
-}
-void loop(){
-  medida = analogRead(A1);
-  Serial.println(medida);
-  delay(300);
+  
+  emon1.current(1, 111.1);             // Current: input pin, calibration.
 }
 
+void loop()
+{
+  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  Serial.println(Irms/2);          // Irms
+}
