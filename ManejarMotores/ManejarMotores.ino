940763c9bf0b405c1f55e39f6f259949f17ee145
chepecarlos <chepecarlos@alswblog.org>
2017-07-13 17:01:38 -0600

    Codigo del ultimo estriming, manejo de motores

--- ManejarMotores/ManejarMotores.ino
+++ ManejarMotores/ManejarMotores.ino
@@ -1,37 +1,30 @@
-#define VELOCIDAD 1700
-
 #include <Servo.h>
-
 #include <IRremote.h>
 
+#define VELOCIDAD 1700
+
 Servo servomotor;
-Servo servomotor2;
 
+//PinIR
 int ReceptorIR = 11;
-
-int derecha = 8;
-
-int izquierda = 9;
-
-int posicion = 6;
-
-int posicion2 = 7;
-
+//PinMotorDC
+int derecha = 53;
+int izquierda = 52;
+//PinMotorServo
+int posicion;
+int PosicionPasada;
+int PinServo = 6;
+//PinMotorPasoPaso
 int steps = 13;
-
 int direccion = 9;
-
-int reset = 10;
-
+int resets = 10;
 int pasos = 2000;
 
 IRrecv irrecv(ReceptorIR);
 decode_results results;
 decode_results Codigos;
 
-void setup()
-
-{
+void setup() {
 
   Serial.begin(9600);
 
@@ -45,26 +38,20 @@ void setup()
 
   pinMode(direccion, OUTPUT);
 
-  pinMode(reset, OUTPUT);
+  pinMode(resets, OUTPUT);
 
-  servomotor.attach(6); //Asociamos el servomotor al pin 3 del Arduino
+  servomotor.attach(PinServo);
 
-  posicion = 0; //Ponemos el servo a 90º al iniciar el programa
-
+  posicion = 0;
+  PosicionPasada = posicion;
 }
 
 
-void loop() // Cuerpo del programa
+void loop() {
 
-{
+  if (irrecv.decode(&Codigos))  {
 
-  if (irrecv.decode(&Codigos))
-
-  {
-
-    switch (Codigos.value)
-
-    {
+    switch (Codigos.value)    {
 
       case 0x10EF10EF:
 
@@ -74,20 +61,19 @@ void loop() // Cuerpo del programa
         Serial.println("");
         break;
 
-      case 0x10EF807F
-          :
-        posicion = 0; 
+      case 0x10EF807F:
+        posicion = 0;
         Serial.print("Posicion de servomotor angular = ");
         Serial.print(posicion);
         Serial.println("");
         break;
 
       case 0x10EFA05F:
-        posicion = posicion + 5; 
+        posicion = posicion + 5;
         if (posicion > 180) {
-          posicion = 180; 
+          posicion = 180;
         }
-       
+
         Serial.print("Posicion angular = ");
         Serial.print(posicion);
         Serial.println("");
@@ -96,74 +82,74 @@ void loop() // Cuerpo del programa
       case 0x10EF00FF:
         posicion = posicion - 5;
         if (posicion < 0) {
-          posicion = 0; 
+          posicion = 0;
         }
-        
+
         Serial.print("Posicion angular = ");
         Serial.print(posicion);
         Serial.println("");
         break;
 
       case 0x10EF20DF:
- 
-          for (int i = 0; i < pasos; i++)     //Equivale al numero de vueltas (200 es 360º grados) o micropasos
-          {
-            digitalWrite(steps, HIGH);  // This LOW to HIGH change is what creates the
-            digitalWrite(steps, LOW); // al A4988 de avanzar una vez por cada pulso de energia.
-            delayMicroseconds(VELOCIDAD);     // Regula la velocidad, cuanto mas bajo mas velocidad.
 
-          }
-
-          digitalWrite(reset, LOW);   //Mientras reset este en LOW el motor permanecerá apagado y no sufrirá. El chip apagará todos los puertos y no leerá comandos.
-          delay(100);
-          digitalWrite(reset, HIGH);   //Cuando reset se encuentre en HIGH el motor arrancará y leerá los comandos enviados.
-          digitalWrite(direccion, LOW);
-
-          for (int i = 0; i < pasos; i++)     //Equivale al numero de vueltas (200 es 360º grados) o micropasos
-          {
-
-            digitalWrite(steps, LOW);   // LOW to HIGH hace que el motor avance ya que da la orden
-            digitalWrite(steps, HIGH);    // al A4988 de avanzar una vez por cada pulso de energia.
-            delayMicroseconds(VELOCIDAD);         // Regula la velocidad, cuanto mas bajo mas velocidad.
-          }
-
-          break;
-
-        case 0x10EFF807:
-
-          digitalWrite(derecha, HIGH);
-          Serial.print("Giro hacia derecha");
-          Serial.println("");
-          break;
-
-        case 0x10EF7887:
-
-          digitalWrite(derecha, LOW);
-          Serial.print("Giro hacia derecha detenido");
-          Serial.println("");
-
-          break;
-
-        case 0x10EFD827:
-
-          digitalWrite(izquierda, HIGH);
-          Serial.print("Giro hacia izquierda");
-          Serial.println("");
-          break;
-
-        case 0x10EF58A7
-            :
-
-          digitalWrite(izquierda, LOW);
-          Serial.print("Giro hacia izquierda detenido");
-          Serial.println("");
-          break;
+        for (int i = 0; i < pasos; i++)             {
+          digitalWrite(steps, HIGH);
+          digitalWrite(steps, LOW);
+          delayMicroseconds(VELOCIDAD);
 
         }
-        servomotor.write(posicion);
 
-        irrecv.resume();
+        digitalWrite(resets, LOW);
+        delay(100);
+        digitalWrite(resets, HIGH);
+        digitalWrite(direccion, LOW);
+
+        for (int i = 0; i < pasos; i++)
+        {
+
+          digitalWrite(steps, LOW);
+          digitalWrite(steps, HIGH);
+          delayMicroseconds(VELOCIDAD);
+        }
+
+        break;
+
+      case 0x10EFF807:
+
+        digitalWrite(derecha, HIGH);
+        digitalWrite(izquierda, LOW);
+        Serial.print("Giro hacia derecha");
+        Serial.println("");
+        break;
+
+      case 0x10EF7887:
+
+        digitalWrite(derecha, LOW);
+        Serial.print("Giro hacia derecha detenido");
+        Serial.println("");
+
+        break;
+
+      case 0x10EFD827:
+
+        digitalWrite(izquierda, HIGH);
+        digitalWrite(derecha, HIGH);
+        Serial.print("Giro hacia izquierda");
+        Serial.println("");
+        break;
+
+      case 0x10EF58A7:
+
+        digitalWrite(izquierda, LOW);
+        Serial.print("Giro hacia izquierda detenido");
+        Serial.println("");
+        break;
 
     }
-
+    if (PosicionPasada != posicion) {
+      servomotor.write(posicion);
+      PosicionPasada = posicion;
+    }
+    irrecv.resume();
   }
+}
