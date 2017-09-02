de30237e4c5a7580afafa841d61ddfdbc494aa80
chepecarlos <chepecarlos@alswblog.org>
2017-09-02 01:51:47 -0600

    Mejorando codigo con mejoras con NeoPixel

--- Proyectos/ConecxionFacebook/PlayingLedWithFacebookNeoPixel/PlayingLedWithFacebookNeoPixel.ino
+++ Proyectos/ConecxionFacebook/PlayingLedWithFacebookNeoPixel/PlayingLedWithFacebookNeoPixel.ino
@@ -12,38 +12,46 @@
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+int Led = 5;
+int CantidadConciones = 0;
+int CantidadFallidas = 0;
+
 //Inforacion de la RED
-const char* ssid     = "ALSW2"; //Nombre de la red
+const char* ssid     = "ChepeCarlosWifi"; //Nombre de la red
 const char* password = "7210-3607"; //Contraseña
 
 //Inforacion de la consulta a facebook
 //Para la contraseña entra a https://developers.facebook.com/tools/explorer/
 const char*  server = "facebook.com";//Servidor
-String AccessToken = "EAACEdEose0cBAGLRwvtmFq89KPHkHBJwhcOtqe2L3Q3CbRqfvXhdUZCiaT7snQDdi5NZCiuZBL1N0aZAI8rKeZBcgBt5wrGIWKlnjcieKGSqaYHxRMSe8ZBsCPsFk2tah5aYKGUZBTRAeBa1HetCJBWq6pPZCgrZA8eaUZBa2JqZA40rNV7OPZAwCErDOLoGSMhZA90MZD";
+String AccessToken = "EAACEdEose0cBAAnzJY8WLeCK6WKHxBhjx0GZBMjZBoZC2C6Sfi4TbsqTBAoMBQ1Vy067awyUceTtPmkgz6HsCd1dko9CGPjVHQS10OsTZC8mKAv2hLFzH2zROfUaEzKBSZBopl0ZBjwdqKebXby6qSaUCq7ZAj4kVZBuOkPkeWtJ8SFemIItKDMlLlUzjZCWZAtX4ZD";
 char* IDStriming = "1401754659879679";//ID del video o foto
 
 WiFiClientSecure client;//Cliente para contartar a facebook
 
-//Pines del LED RGB
-int LedRojo = 13;
-int LedAzul = 14;
-int LedVerde = 12;
-
 //Respuesta con Facebook
 String Dato;
+
+String Mensaje[9] = {
+  "\"error\"",
+  "#ROJO",
+  "#AZUL",
+  "#VERDE",
+  "#AMARILLO",
+  "#MORADO",
+  "#CIAN",
+  "#FIESTA",
+  "#AURORA"
+};
+
+int CantidadMensajes = 9;
 
 void setup() {
   //Inicializa la comunicacion Serial
   Serial.begin(115200);
   delay(100);
 
-  //Activa los pines de los y los apaga
-  pinMode(LedRojo, OUTPUT);
-  pinMode(LedAzul, OUTPUT);
-  pinMode(LedVerde, OUTPUT);
-  digitalWrite(LedRojo, 0);
-  digitalWrite(LedVerde, 0);
-  digitalWrite(LedAzul, 0);
+  pinMode(Led, OUTPUT);
+  digitalWrite(Led, 1);
 
   Serial.print("Intentando Conectar a: ");
   Serial.println(ssid);
@@ -54,6 +62,7 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     WiFi.begin(ssid, password);
     Serial.print(".");
+    digitalWrite(Led, !digitalRead(Led));
     //Espera 1 segundo para intenar de nuevo
     delay(500);
   }
@@ -62,6 +71,7 @@ void setup() {
   Serial.println(ssid);
 
   pixels.begin();
+  pixels.clear();
   ColorNeo(100, 100, 100);
 
   //Preparar la consulta para facebook
@@ -70,7 +80,6 @@ void setup() {
   Dato.concat("?fields=comments.limit(4).order(reverse_chronological)&access_token=");
   Dato.concat(AccessToken);
   Dato.concat(" HTTP/1.0");
-
 }
 
 void loop() {
@@ -78,7 +87,7 @@ void loop() {
   //Comandos validos
   //#ROJO #AZUL #VERDE
   ConsultaFB();
-  delay(500);
+  delay(1000);
 }
 
 //Consulta a Facebook y verifica cual color es el ultimo
@@ -86,71 +95,61 @@ void loop() {
 void ConsultaFB() {
   String Texto = ConsutaFacebook(Dato);
 
-  //Inicalizar las varialbes
-  int ValorRojo = -1;
-  int ValorAzul = -1;
-  int ValorVerde = -1;
-  int ValorAmarillo = -1;
-  int ValorError = -1;
+  int SelecionColor = -1;
+  int SelecionValor = Texto.length();
+  int SelecionActual = -1;
 
-
-  //Muestra datos y el Texto que recibimos
   Serial.println(Texto);
   Serial.print("Cantidad de texto ");
   Serial.println(Texto.length());
 
-  //Obtiene las primera pocion que aparece los comandos
-  //Si no lo encuentra devolvera -1
-  ValorAzul = Texto.indexOf("#AZUL");
-  ValorRojo = Texto.indexOf("#ROJO");
-  ValorVerde = Texto.indexOf("#VERDE");
-  ValorAmarillo = Texto.indexOf("#AMARILLO");
-  ValorError = Texto.indexOf("\"error\"");
-
-  if (ValorError > 0 && ValorError < 700) {
-    Serial.println("Exite un problema, por favor revisa el ID o el Token");
-    Serial.print("Error Fatal ");
-    Serial.println(ValorError);
-    digitalWrite(LedRojo,  1);
-    digitalWrite(LedAzul,  0);
-    digitalWrite(LedVerde, 0);
-    delay(500);
-    digitalWrite(LedRojo,  0);
-    delay(500);
-    digitalWrite(LedRojo, 1);
-    delay(500);
-    digitalWrite(LedRojo, 0);
-    delay(500);
-    digitalWrite(LedRojo, 1);
-    delay(500);
-    digitalWrite(LedRojo, 0);
-    Serial.println();
-    Serial.println("Intentar de nuevo");
-    Serial.println();
+  for (int i = 0; i < CantidadMensajes; i++) {
+    SelecionActual = Texto.indexOf(Mensaje[i]);
+    if (SelecionActual != -1) {
+      Serial.print("Encontrado ");
+      Serial.print(Mensaje[i]);
+      Serial.print(" Posicion ");
+      Serial.println(SelecionActual);
+      if (EsMenor(SelecionActual, SelecionValor)) {
+        SelecionColor = i;
+        SelecionValor = SelecionActual;
+      }
+    }
   }
-  else {
-    //Imprime em Serial en que dato esta
-    Serial.print("Rojo: ");
-    Serial.print(ValorRojo);
-    Serial.print(" Azul: ");
-    Serial.print(ValorAzul);
-    Serial.print(" Verde: ");
-    Serial.print(ValorVerde);
-    Serial.print(" Amaillo: ");
-    Serial.println(ValorAmarillo);
-    //Busca cual es valor mas pequeño entre todos descartando uno por uno
-    if (Menor(ValorAmarillo, ValorRojo) && Menor(ValorAmarillo, ValorAzul) && Menor(ValorAmarillo, ValorVerde)) {
-      ColorNeo(100, 0, 100);
-    }
-    if (Menor(ValorRojo, ValorAzul) && Menor(ValorRojo, ValorVerde)) {
+
+  Serial.print("Selcionado ");
+  Serial.print(SelecionColor);
+  Serial.print(" - ");
+  Serial.println(Mensaje[SelecionColor]);
+
+  switch (SelecionColor) {
+    case 1://Rojo
       ColorNeo(255, 0, 0);
-    }
-    else if (Menor(ValorAzul, ValorVerde)) {
-      ColorNeo(0, 255, 0);
-    }
-    else if (ValorVerde != -1) {
+      break;
+    case 2://Azul
       ColorNeo(0, 0, 255);
-    }
+      break;
+    case 3://Verde
+      ColorNeo(255, 0, 0);
+      break;
+    case 4://Amarillo
+      ColorNeo(255, 255, 0);
+      break;
+    case 5://Morado
+      ColorNeo(255, 0, 255);
+      break;
+    case 6:
+      ColorNeo(0, 255, 255);
+      break;
+    case 7://Fiesta
+      ColorNeo(255, 0, 0);
+      break;
+    case 8://AURORA
+      ColorNeo(0, 0, 255);
+      break;
+    default:
+      Serial.println("Error Fatal");
+      break;
   }
 }
 
@@ -159,9 +158,15 @@ void ConsultaFB() {
 String ConsutaFacebook(String URL) {
   String Texto = "\"error\"";
   //Valor Inicial
-  Serial.println("\nEmpezando coneccion con el servidor...");
-  if (!client.connect(server, 443))
+  Serial.print("\nEmpezando coneccion con el servidor...");
+  Serial.print(CantidadConciones );
+  Serial.print(" - ");
+  Serial.println(CantidadFallidas);
+  CantidadConciones ++;
+  if (!client.connect(server, 443)) {
     Serial.println("Conexion Fallida");
+    CantidadFallidas++;
+  }
   else {
     Serial.println("Conexion Exitosa");
 
@@ -172,10 +177,13 @@ String ConsutaFacebook(String URL) {
     client.println();
 
     //Esperando que nos envien todo los datos el servidor
+    int t = 0;
     Serial.print("Esperando Respuesta ");
-    while (!client.available()) {
-      delay(500); //
+    while (!client.available() && t < 10) {
+      delay(250); //
       Serial.print(".");
+      digitalWrite(Led, !digitalRead(Led));
+      t++;
     }
 
     //Si nos mandaron datos lo guandamos en Texto
@@ -184,7 +192,7 @@ String ConsutaFacebook(String URL) {
     }
 
     //Nos Desconectamos del servidor
-    if (!client.connected()) {
+    if (!client.connected() || t > 5) {
       Serial.println();
       Serial.println("Desconectar del servidor");
       client.stop();
@@ -195,9 +203,8 @@ String ConsutaFacebook(String URL) {
 
 //Busca si el Valor1 es menor que el Valor2 o
 //no fue encontrado que el valor sera -1
-bool Menor(int Valor1, int Valor2) {
-
-  if ((Valor1 == -1 && Valor2 == -1) || Valor1 == -1) {
+bool EsMenor(int Valor1, int Valor2) {
+  if (Valor1 == -1) {
     return false;
   }
   else if (Valor2 == -1 || Valor1 < Valor2) {
@@ -216,6 +223,9 @@ void ColorNeo(int Rojo, int Azul, int Verde) {
   for (int i = 0; i < NUMPIXELS; i++) {
     // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(Rojo, Azul, Verde)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(1);                     // one partial workaround
+    portDISABLE_INTERRUPTS();     // the other workaround
+    pixels.show();
+    portENABLE_INTERRUPTS();
   }
 }
