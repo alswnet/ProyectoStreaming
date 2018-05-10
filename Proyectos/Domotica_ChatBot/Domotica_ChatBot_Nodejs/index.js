ff10b4857c8fc9210d671a459f27fd1ed1b6637f
chepecarlos <chepecarlos@alswblog.org>
2018-05-09 23:09:09 -0600

    Mejorando codigo para agregar otro focos

--- Proyectos/Domotica_ChatBot/Domotica_ChatBot_Nodejs/index.js
+++ Proyectos/Domotica_ChatBot/Domotica_ChatBot_Nodejs/index.js
@@ -10,17 +10,39 @@ const bot = new TelegramBot(token, {
   polling: true
 });
 
-
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   var Mensaje = msg.text;
   if (Mensaje == "Encender1") {
     bot.sendMessage(chatId, 'Encendiendo Lugar 1');
     port.write('a');
-  }
-  else if(Mensaje == "Apagar1"){
+  } else if (Mensaje == "Apagar1") {
     bot.sendMessage(chatId, 'Apagando Lugar 1');
     port.write('b');
   }
-
+  if (Mensaje == "Encender2") {
+    bot.sendMessage(chatId, 'Encendiendo Lugar 2');
+    port.write('c');
+  } else if (Mensaje == "Apagar2") {
+    bot.sendMessage(chatId, 'Apagando Lugar 2');
+    port.write('d');
+  }
+  if (Mensaje == "Encender3") {
+    bot.sendMessage(chatId, 'Encendiendo Lugar 3');
+    port.write('e');
+  } else if (Mensaje == "Apagar3") {
+    bot.sendMessage(chatId, 'Apagando Lugar 3');
+    port.write('f');
+  }
+  if (Mensaje == "Encendertodo") {
+    bot.sendMessage(chatId, 'Encendiendo todas las luses');
+    port.write('a');
+    port.write('b');
+    port.write('c');
+  } else if (Mensaje == "Apagartodo") {
+    bot.sendMessage(chatId, 'Apagando todas las luses');
+    port.write('b');
+    port.write('e');
+    port.write('f');
+  }
 });
